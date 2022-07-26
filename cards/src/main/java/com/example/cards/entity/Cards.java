25b3a59de4ce25b991caea2b1a0b0da4a3b70c9b
Narendra B <narendraborra09@gmail.com>
2022-07-26 13:10:22 +0530

    @data annotation added

--- cards/src/main/java/com/example/cards/entity/Cards.java
+++ cards/src/main/java/com/example/cards/entity/Cards.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -16,6 +17,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Data
 public class Cards {
 
 	@Id
